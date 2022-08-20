47032a8f049e7f8420789679d5d94b45912964b2
Rodrigo Orlando <92063235+rodrigoorlandogti@users.noreply.github.com>
2022-08-20 00:00:20 -0300

    Alterando texto do banner!

--- js/principal.js
+++ js/principal.js
@@ -1,6 +1,5 @@
-var banners = ["Os melhores do Brasil!", "Qualidade e preço baixo!"];
+var banners = ["Do lixo ao luxo!", "Reaproveitar é aproveitar!"];
 var bannerAtual = 0;
-
 function trocaBanner(){
     bannerAtual = (bannerAtual + 1)%2;
     document.querySelector('h2#mensagem').textContent = banners[bannerAtual]
